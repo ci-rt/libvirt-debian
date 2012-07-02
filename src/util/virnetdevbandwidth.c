6cf501ca9d76d417768e7a46cd6c8c1d36eedf7c
Guido Günther <agx@sigxcpu.org>
2012-07-02 08:41:30 +0200

    New upstream version 0.9.13

--- src/util/virnetdevbandwidth.c
+++ src/util/virnetdevbandwidth.c
@@ -26,7 +26,6 @@
 #include "command.h"
 #include "memory.h"
 #include "virterror_internal.h"
-#include "ignore-value.h"
 
 #define VIR_FROM_THIS VIR_FROM_NONE
 
@@ -136,7 +135,7 @@ virNetDevBandwidthSet(const char *ifname,
         virCommandAddArgList(cmd, "filter", "add", "dev", ifname, "parent",
                              "ffff:", "protocol", "ip", "u32", "match", "ip",
                              "src", "0.0.0.0/0", "police", "rate", average,
-                             "burst", burst, "mtu", burst, "drop", "flowid",
+                             "burst", burst, "mtu", "64kb", "drop", "flowid",
                              ":1", NULL);
 
         if (virCommandRun(cmd, NULL) < 0)
